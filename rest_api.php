c73781e288b12bb0bf90a61a6d5ae7c91d8fe3c0
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2023-07-02 15:18:16 -0400

    rest_api.php

---
+++ rest_api.php
@@ -0,0 +1,48 @@
+add_action( 'rest_api_init', function() {
+  register_rest_route( 'news-ticker/v1', '/add', [
+    'methods' => 'POST',
+    'callback' => 'news_ticker_add_new',
+    'permission_callback' => 'restCheckUser',
+  ] );
+} );
+
+function restCheckUser(WP_REST_Request $request) {
+    if ('token_for_rest_api' === $request->get_param('token')) {
+         return true;
+    }
+    return false;
+}
+
+function news_ticker_add_new( $request ) {
+	
+  	$params = wp_parse_args( $request->get_params(), [
+    	'title' => '',
+		'media_id' => '',
+    	'video' => '',
+  	] );	
+	
+	$title = $params['title'];
+	$author_name = $params['author_name'];
+	$media_id = $params['media_id'];
+	$is_video = $params['video'];
+	
+	// publish post on website
+	$post_id = wp_insert_post( array (
+		'post_type' => 'news-ticker',
+		'post_title' => $title,
+		'post_status' => 'publish',
+		'post_author' => '1',
+	) );
+		
+	if ( $media_id != 0 ) {
+		
+		// if this is video
+		if ( $is_video == "true" ) {
+			update_post_meta( $post_id, 'video', $media_id );
+		} else {
+			// if this is image
+			update_post_meta( $post_id, 'image', $media_id );
+		}
+		
+	} 
+}
